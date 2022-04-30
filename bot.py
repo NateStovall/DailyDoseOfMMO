22d65d6011e4b93f3de99577c029c1e7b29b2993
Christian Stovall <cstovall@blizzard.com>
2022-04-30 12:19:47 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.content == '!wow':
+        await message.channel.send("im cool")
+
+client.run(TOKEN)
